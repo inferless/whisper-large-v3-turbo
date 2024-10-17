39b1f14df3884a79b97860f73fb9c7cbe1a713f8
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-10-17 22:00:58 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -3,6 +3,36 @@ INPUT_SCHEMA = {
         'datatype': 'STRING',
         'required': True,
         'shape': [1],
-        'example': ["http://thepodcastexchange.ca/s/Porsche-Macan-July-5-2018-1.mp3"]
+        'example': ["https://github.com/rbgo404/Files/raw/main/jeanNL.mp3"]
+    },
+    "return_timestamps": {
+        'datatype': 'BOOL',
+        'required': False,
+        'shape': [1],
+        'example': [True]
+    },
+    "return_timestamps": {
+        'datatype': 'INT64',
+        'required': False,
+        'shape': [1],
+        'example': [400]
+    },
+    "language": {
+        'datatype': 'STRING',
+        'required': False,
+        'shape': [1],
+        'example': ['english']
+    },
+    "task": {
+        'datatype': 'STRING',
+        'required': False,
+        'shape': [1],
+        'example': ['transcribe']
+    },
+    "temperature": {
+        'datatype': 'FP64',
+        'required': False,
+        'shape': [1],
+        'example': [0.5]
     }
 }
