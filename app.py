b0d0e9c22619380918f850d2ec65ec38fc81edbd
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-10-18 13:04:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,42 +26,43 @@ class InferlessPythonModel:
         )
 
     def infer(self, inputs):
+        # Extracting inputs with default values
         audio_url = inputs["audio_url"]
-        return_timestamps = inputs.get("return_timestamps",False)
-        max_new_tokens = inputs.get("max_new_tokens",None)
-        language = inputs.get("language",None)
-        task = inputs.get("task",None)
-        temperature = inputs.get("temperature",None)
-        
-        result = self.pipe(audio_url,
-              return_timestamps=return_timestamps,
-              generate_kwargs={"max_new_tokens": max_new_tokens,
-                               "language": language,
-                               "task": task,
-                               "temperature": temperature,
-                              }
+        return_timestamps = inputs.get("return_timestamps", "word") # Can be True, False, or "word"
+        max_new_tokens = inputs.get("max_new_tokens")
+        language = inputs.get("language")
+        task = inputs.get("task")
+        temperature = inputs.get("temperature")
+
+        # Convert return_timestamps if needed
+        return_timestamps = return_timestamps == "True" if return_timestamps in ["True", "False"] else return_timestamps
+        # Call the pipeline with necessary parameters
+        result = self.pipe(
+        audio_url,
+        return_timestamps=return_timestamps,
+        generate_kwargs={
+            "max_new_tokens": max_new_tokens,
+            "language": language,
+            "task": task,
+            "temperature": temperature,
+            },
         )
-        
+        # Prepare the output based on the return_timestamps condition
         if not return_timestamps:
-            return {"output": result["text"]}
-        else:
-            from_timestamp = []
-            to_timestamp = []
-            chunk_text = []
+            return {"output_text": result["text"]}
 
-            for chunk in result['chunks']:
-                print(chunk['timestamp'])
-                print(chunk['text'])
-                from_timestamp.append(chunk['timestamp'][0])
-                to_timestamp.append(chunk['timestamp'][1])
-                chunk_text.append(chunk['text'])
+        # Extract timestamps and chunk text
+        from_timestamp, to_timestamp, chunk_text = zip(
+            *[(chunk['timestamp'][0], chunk['timestamp'][1], chunk['text']) for chunk in result['chunks']]
+        )
 
-            return {
-                "output":result["text"],
-                "from_timestamp": from_timestamp,
-                "to_timestamp":to_timestamp,
-                "chunk_text":chunk_text
-                }
+        return {
+            "output_text": [result["text"]],
+            "from_timestamp": list(from_timestamp),
+            "to_timestamp": list(to_timestamp),
+            "chunk_text": list(chunk_text),
+        }
+
 
     def finalize(self):
         self.pipe = None
